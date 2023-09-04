d7402c126b562dde4217fb8c550d719a6eb39b74
jedaydiya <joloanjed@gmail.com>
2023-09-04 18:43:24 +0800

    Added Footer Component

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,9 +1,52 @@
+import { Separator } from "@/components/ui/separator";
 export default function Footer() {
   return (
     <>
       <footer className="mx-auto w-full max-w-7xl px-4 py-3">
-        <div className="bg-zinc-950 rounded-sm px-4">
-          <p className="text-white">This the footer</p>
+        <div className="rounded-lg bg-zinc-950 px-10 py-8">
+          <p className="text-4xl font-bold text-white">hiraya</p>
+          <div className="grid grid-cols-3">
+            <div>
+              <p className="mt-2 text-lg font-semibold text-white">
+                Discover matcha perfection
+              </p>
+              <p className="mt-2 text-lg font-semibold text-white">
+                © 2010 — 2020
+              </p>
+              <p className="mt-2 text-lg font-semibold text-white">
+                Privacy - Terms
+              </p>
+            </div>
+            <div>
+              <p className="py-2 text-lg font-semibold text-gray-300">
+                EXPLORE
+              </p>
+              <p className="text-lg font-semibold text-white">Features</p>
+              <p className="mt-2 text-lg font-semibold text-white">
+                Testimonials
+              </p>
+              <p className="mt-2 text-lg font-semibold text-white">Contacts</p>
+            </div>
+
+            <div>
+              <p className="py-2 text-lg font-semibold text-gray-300">
+                LET'S CONNECT
+              </p>
+              <p className="text-lg font-semibold text-white">Github</p>
+              <p className="mt-2 text-lg font-semibold text-white">
+                Twitter
+              </p>
+              <p className="mt-2 text-lg font-semibold text-white">Instagram</p>
+            </div>
+
+          </div>
+          <Separator className="mt-8" />
+          <p className="text-md mt-10 font-semibold text-secondary">
+            Feedback? Suggestions Features you'd like to see? Contact Me
+          </p>
+          <p className="text-md mt-2 font-semibold text-secondary">
+            Made with luv by jedaydiya
+          </p>
         </div>
       </footer>
     </>
